4ee9c0a85207588a36a2231088b847208aea2ce5
Alex Yang <himself65@outlook.com>
2026-02-22 19:11:57 +0800

    chore: enable sourcemap for beta

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	productionBrowserSourceMaps: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL === 'beta.better-hub.com',
 	devIndicators: false,
 	serverExternalPackages: ["@prisma/client"],
 	experimental: {
