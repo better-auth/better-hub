65157501862862e4aa46ab536667e05be3e802e9
Alex Yang <himself65@outlook.com>
2026-02-22 23:40:30 +0800

    fix: add server external

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	devIndicators: false,
-	serverExternalPackages: ["@prisma/client"],
+	serverExternalPackages: ["@prisma/client", "resumable-stream"],
 	experimental: {
 		staleTimes: {
 			dynamic: 300,
