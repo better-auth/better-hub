7d89c05d14b31ab35cc5574bc2391054c1ddcb3f
justjxke <Jake0300@Proton.me>
2026-03-15 22:24:06 +0000

    remove todo, didn't realize it was working without redirecting

--- apps/web/src/app/(app)/repos/[owner]/[repo]/releases/download/[tag]/[...filename]/route.ts
+++ apps/web/src/app/(app)/repos/[owner]/[repo]/releases/download/[tag]/[...filename]/route.ts
@@ -9,7 +9,6 @@ export async function GET(
 	const { owner, repo, tag, filename } = await context.params;
 
 	// this route basically redirects users to the real github asset URL, if the extension is on it can loop unless that URL is allowed listed in extension rules
-	// TODO: Consider some sort of method to download releases from github directly while staying on better-hub, no redirects required
 	const githubUrl = `https://github.com/${owner}/${repo}/releases/download/${encodeURIComponent(tag)}/${filename.join("/")}`;
 	return NextResponse.redirect(githubUrl, { status: 302 });
 }
